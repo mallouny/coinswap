f29b124c0080daaf09f02e09be5629dfbbeea594
mallouny <mallouny@icloud.com>
2025-11-13 18:48:51 +0200

    Initial Commit

---
+++ coinswap/coinswapApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinswapApp.swift
+//  coinswap
+//
+//  Created by vladislav mallouny on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct coinswapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
